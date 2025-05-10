247f7326929549ca8423bf235e3a3a935bc353f3
Yonier Garcia <yoniermosquera55@gmail.com>
2025-05-10 01:41:01 -0500

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Formulario-con-React-Hook-Form-y-Zod/",
   plugins: [react(), tailwindcss()],
 });
